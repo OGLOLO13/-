5f20fd4ca20e05976ac473baa00c2f3133e46748
OGLOLO13 <makskyl30@gmail.com>
2024-06-28 00:21:27 +0300

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,13 @@
+print("1st program")
+print(9**0.5)
+print(3*5)
+print("2nd program")
+print(9.99>9.98 and 1000 != 1000.1)
+print("3rd program")
+print(type(1234 and 5678))
+print((23+67)/2)
+print("4th program")
+print(13==13 or (int (13.42)==42.13))
+print(42==42 or (int (42.13)==13.42))
+
+
