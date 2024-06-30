76e2a491655dbc94c9c71c6f8c1e07fbbe90b42e
OGLOLO13 <makskyl30@gmail.com>
2024-06-30 13:38:07 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,21 @@
+name="Maxim"
+print(name)
+age=24
+print(age)
+new_age=(age+1)
+print(new_age)
+is_student=(True)
+print(is_student)
+print('Name:',name)
+print('Age:',age)
+print('New age:',new_age)
+print('Is Student:',is_student)
+
+a='Name:Maxim'
+print(name)
+b="Age: "+ str (24)
+print(b)
+c="New age:"+ str(24+1)
+print(c)
+d= "Is Student:"+ str(True)
+print(d)
