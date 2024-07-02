904bc5bbda5f6c5ca6a38ef320e9deec4d72ab06
OGLOLO13 <makskyl30@gmail.com>
2024-07-02 21:52:10 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+my_string = input("Привет! С какого ты города? ")
+print(type (my_string))
+print(len(my_string))
+
+my_string = "монитор от фирмы aoc"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[19])
+
