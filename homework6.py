bb1d88a1eef977a0c6432c69982a33ea41f4c7ab
OGLOLO13 <makskyl30@gmail.com>
2024-07-04 22:16:09 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,18 @@
+#my_dict = {"Максим": 1999, "Василий" : 2001, "Олег" : 2003}
+#print(my_dict)
+#print(my_dict ["Олег"])
+#print(my_dict.get("Анна"))
+#my_dict.update({"Анатолий" : 1998,"Александр": 2002} )
+#a = my_dict.pop("Василий")
+#print(a)
+#print(my_dict)
+
+set =  {1, "Часы", 1, 3, 3, "Часы", "Телефон"}
+print(set)
+set.add("Цветок")
+set.add("Ножницы")
+set.remove("Часы")
+print(set)
+
+
+
