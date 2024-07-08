0d343ca8269fab945d0ca60db7e9c01015b969f6
OGLOLO13 <makskyl30@gmail.com>
2024-07-09 00:07:12 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,24 @@
+a,b,c =map(int, input("Введите три целых числа - "))
+if a == b == c:
+    print(3)
+elif a == b or a == c or b == c:
+    print(2)
+else:
+    print(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
