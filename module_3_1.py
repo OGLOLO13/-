75da4377c4f17f077753e6c3e4f23019ab373eaf
OGLOLO13 <makskyl30@gmail.com>
2024-07-14 21:49:27 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -21,8 +21,8 @@ def is_contains(string, list_to_search):
     return result
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) 
+print(is_contains('cycle', ['recycling', 'cyclic'])) 
 print(calls)
 
 
